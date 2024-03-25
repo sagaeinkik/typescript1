50443f2a36c45d14a262d0846ceb949357af9b05
Saga Einarsdotter Kikajon <saga@kikajon.se>
2024-03-25 17:12:44 +0100

    Gjort om koden

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,4 +1,5 @@
-//Hämta alla inputfält och knappar
+/* DOM-VARIABLER */
+//Formulär
 const courseCodeInput = <HTMLInputElement>document.getElementById('kurskod');
 const courseNameInput = <HTMLInputElement>document.getElementById('kursnamn');
 const progressionInput = <HTMLInputElement>document.getElementById('progression');
@@ -12,257 +13,277 @@ const error2 = <HTMLSpanElement>document.getElementById('error2');
 const error3 = <HTMLSpanElement>document.getElementById('error3');
 const error4 = <HTMLSpanElement>document.getElementById('error4');
 
+//Håller koll på vilken kurs som redigeras, om någon: använd för att styra vilkn funktion vid submit av formulär
+let editingCourseIndex: number | null = null;
+
+//Interface för nya kurser
 interface CourseInterface {
     kurskod: string;
     kursnamn: string;
     progression: string;
     kursplan: string;
 }
-//Håller koll på redigering:
-let isEditing: boolean | null = null;
 
-//Lägg på event listener om isEditing är false eller null
-if (!isEditing) {
-    submitBtn.addEventListener('click', (e: Event) => {
-        addCourse(e);
-    });
-}
-//Skriv ut kurser från localstorage när fönstret har laddat
-window.addEventListener('load', printCourses);
-
-// Hämta befintliga kurser från localStorage, om det inte finns, skapa en tom lista, lagra globalt
+//Hämta kurser från localStorage
 let courses: CourseInterface[] = JSON.parse(localStorage.getItem('courses') || '[]');
 
-//Funktion som kollar om kurskod finns
+/* HÄNDELSEHANTERARE */
+//Skriv ut kurser när sidan laddats
+window.addEventListener('load', printCourses);
+
+//Vid submit
+courseForm.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+    // Om ingen kurs redigeras, lägg till en ny kurs
+    if (editingCourseIndex === null) {
+        addCourse();
+        // Annars uppdatera kursen
+    } else {
+        updateCourse();
+    }
+});
+
+// Funktion för att returnera om kurskoden är unik
 function uniqueCode(code: string): boolean {
+    //Loopa igenom kurserna och se om det finns mer än 1 med samma kod
     return courses.every((course) => course.kurskod !== code);
 }
 
-//Funktion som lägger till kurser
-function addCourse(e: Event): void {
-    e.preventDefault();
-    //Lagra värdena från input
-    const kurskod: string = courseCodeInput.value;
-    const kursnamn: string = courseNameInput.value;
-    const progression: string = progressionInput.value.toUpperCase();
-    const kursplan: string = coursePlanInput.value;
-
-    //Hitta index
-    const index: number = courses.findIndex((course) => course.kurskod === kurskod);
-
-    //Kolla om det är redigering eller inte, lägg till kursen då
-    if (!isEditing) {
-        //Felmeddelanden
-        //Fältet är tomt
-        if (kurskod.length < 1) {
-            error1.innerHTML = 'Du måste ange en kurskod';
-            error2.innerHTML = '';
-            error3.innerHTML = '';
-            error4.innerHTML = '';
-
-            return;
-        } //Ej unik kurskod
+//Funktion för att validera input!
+function validateInput(
+    kurskod: string,
+    kursnamn: string,
+    progression: string,
+    kursplan: string
+): boolean {
+    // Kolla längd på kurskod
+    if (kurskod.length < 1) {
+        error1.textContent = 'Du måste ange en kurskod';
+        error2.textContent = '';
+        error3.textContent = '';
+        error4.textContent = '';
+        return false;
+    }
+    //Kolla unikhet på kurskod om vi lägger till en ny kurs (inte annars)
+    if (editingCourseIndex === null) {
         if (!uniqueCode(kurskod)) {
             error1.innerHTML = 'Kurskoden måste vara unik';
             error2.innerHTML = '';
             error3.innerHTML = '';
             error4.innerHTML = '';
-
-            return;
+            return false;
         }
-        //Fältet är tomt
-        if (kursnamn.length < 1) {
-            error1.innerHTML = '';
-            error2.innerHTML = 'Du måste ange ett kursnamn';
-            error3.innerHTML = '';
-            error4.innerHTML = '';
-            return;
-        }
-        //Fältet är tomt
-        if (progression.length < 1) {
-            error1.innerHTML = '';
-            error2.innerHTML = '';
-            error3.innerHTML = 'Du måste fylla i kursens progression.';
-            error4.innerHTML = '';
-            return;
-        }
-        //Progression är inte A, b eller c (jag gick efter uppgiftsbeskrivningen och inte forumet, sorry)
-        if (progression !== 'A' && progression !== 'B' && progression !== 'C') {
-            error1.innerHTML = '';
-            error2.innerHTML = '';
-            error3.innerHTML = 'Kursens progression måste vara A, B eller C';
-            error4.innerHTML = '';
-            return;
-        }
-        //Fältet är tomt
-        if (kursplan.length < 1) {
-            error1.innerHTML = '';
-            error2.innerHTML = '';
-            error3.innerHTML = '';
-            error4.innerHTML = 'Du måste ange en länk till kursplanen.';
-            return;
-        }
-        //inte url
-        if (!kursplan.includes('http') || !kursplan.includes('.')) {
-            error1.innerHTML = '';
-            error2.innerHTML = '';
-            error3.innerHTML = '';
-            error4.innerHTML = 'Kursplanen måste vara URL.';
-            return;
-        }
-
-        //Om allt gått bra hittills:
-        //Ny kurs
-        const newCourse: CourseInterface = {
-            kurskod,
-            kursnamn,
-            progression,
-            kursplan,
-        };
-        //lägg till i localstorage
-        courses.push(newCourse);
-        localStorage.setItem('courses', JSON.stringify(courses));
-        resetFields();
     }
-    //skriv ut kurser till DOM
+    // Kolla kursnamn
+    if (kursnamn.length < 1) {
+        error1.textContent = '';
+        error2.textContent = 'Du måste ange ett kursnamn';
+        error3.textContent = '';
+        error4.textContent = '';
+        return false;
+    }
+    // Kolla progression
+    if (!['A', 'B', 'C'].includes(progression)) {
+        error1.textContent = '';
+        error2.textContent = '';
+        error3.textContent = 'Kursens progression måste vara A, B eller C';
+        error4.textContent = '';
+        return false;
+    }
+    // Kolla att kursplan är url
+    if (kursplan.length < 1 || !kursplan.includes('http') || !kursplan.includes('.')) {
+        error1.textContent = '';
+        error2.textContent = '';
+        error3.textContent = '';
+        error4.textContent =
+            'Du måste ange en giltig länk till kursplanen med protokoll (http/https) och TLD (.se, .com osv)';
+        return false;
+    }
+
+    // Nollställ fel
+    error1.textContent = '';
+    error2.textContent = '';
+    error3.textContent = '';
+    error4.textContent = '';
+
+    //Om vi har kommit såhär långt är allt grett!
+    return true;
+}
+
+/* Funktion för att lägga till kurs */
+function addCourse(): void {
+    //Variabler för värdena i fälten
+    const kurskod: string = courseCodeInput.value;
+    const kursnamn: string = courseNameInput.value;
+    const progression: string = progressionInput.value.toUpperCase();
+    const kursplan: string = coursePlanInput.value;
+
+    // Om validering inte returnerar true, abort mission
+    if (!validateInput(kurskod, kursnamn, progression, kursplan)) {
+        return;
+    }
+
+    // Annars lägg till nya kursen enligt interface
+    const newCourse: CourseInterface = {
+        kurskod,
+        kursnamn,
+        progression,
+        kursplan,
+    };
+    //Peta in i arrayen och uppdatera storage
+    courses.push(newCourse);
+    localStorage.setItem('courses', JSON.stringify(courses));
+
+    // Återställ formuläret
+    resetFields();
+    // Uppdatera kurslistan
     printCourses();
 }
 
-//Funktion som skriver ut kurserna
+/* Funktion för att skriva ut kurserna till DOM */
 function printCourses(): void {
     const courseList = <HTMLDivElement>document.querySelector('div.courselist');
-    //Rensa listan först
-    courseList.innerHTML = '';
-    //Meddelande om det inte finns kurser
-    if (courses.length < 1) {
+    courseList.innerHTML = ''; // Rensa listan
+
+    if (courses.length === 0) {
         courseList.innerHTML = '<p>Det finns inga kurser att visa ännu...</p>';
     } else {
-        //Loopa igenom
+        // Loopa igenom och skriv ut varje kurs
         courses.forEach((kurs, index) => {
-            //Skapa ett index så man kan ta bort kurser i slumpmässig ordning
-            const courseId: string = `course_${index}`;
-            //Skapa elementen:
             const courseDiv = <HTMLDivElement>document.createElement('div');
             courseDiv.classList.add('course');
+
             //Kurskod
             const codeP = <HTMLParagraphElement>document.createElement('p');
-            codeP.appendChild(document.createTextNode(kurs.kurskod));
+            codeP.textContent = kurs.kurskod;
             courseDiv.appendChild(codeP);
+
             //Kursnamn
             const nameP = <HTMLParagraphElement>document.createElement('p');
-            nameP.appendChild(document.createTextNode(kurs.kursnamn));
+            nameP.textContent = kurs.kursnamn;
             courseDiv.appendChild(nameP);
+
             //Progression
             const progP = <HTMLParagraphElement>document.createElement('p');
-            progP.appendChild(document.createTextNode(kurs.progression));
+            progP.textContent = kurs.progression;
             courseDiv.appendChild(progP);
-            //Url
+
+            //Länk till kursplan
             const linkA = <HTMLAnchorElement>document.createElement('a');
             linkA.href = kurs.kursplan;
-            linkA.appendChild(document.createTextNode('Länk till kursplan'));
+            linkA.setAttribute('target', '_blank');
+            linkA.textContent = 'Länk till kursplan';
             courseDiv.appendChild(linkA);
-            //Div runt knappar
+
+            //Div för knappar
             const controlsDiv = <HTMLDivElement>document.createElement('div');
             controlsDiv.classList.add('controls');
-            //Knappar
+
+            //Ändra-knappen:
             const editBtn = <HTMLButtonElement>document.createElement('button');
             editBtn.classList.add('change');
-            editBtn.appendChild(document.createTextNode('Ändra kurs'));
-            controlsDiv.appendChild(editBtn);
+            editBtn.textContent = 'Ändra kurs';
+            //Lägg på händelsehanterare
+            editBtn.addEventListener('click', (e: Event) => {
+                //Tilldela redigeringsindex till variabeln
+                editingCourseIndex = index;
+
+                //kalla på funktion som fyller i fälten
+                loadValuesForEdit(kurs);
+                // Ändra text på knappen
+                submitBtn.value = 'Spara ändringar';
+            });
+
+            //Radera-knappen
             const deleteBtn = <HTMLButtonElement>document.createElement('button');
             deleteBtn.classList.add('delete');
-            deleteBtn.appendChild(document.createTextNode('Ta bort'));
+            deleteBtn.textContent = 'Ta bort';
+            //Lägg till händelsehanterare
+            deleteBtn.addEventListener('click', () => {
+                /* När man använde index i foreach-loopen så blev det skumt när man skulle ta bort kurser i slumpmässig ordning,
+                så lagra kurskod i variabel och använd för att filtrera bort istället */
+                const courseCodeToRemove: string = kurs.kurskod;
+                courses = courses.filter(
+                    (course: CourseInterface) => course.kurskod !== courseCodeToRemove
+                );
+                // Uppdatera storage och ta bort diven från DOM
+                localStorage.setItem('courses', JSON.stringify(courses));
+                courseDiv.remove();
+            });
+
+            //Peta in knappar i div.controls, peta in div.controls i DOM
+            controlsDiv.appendChild(editBtn);
             controlsDiv.appendChild(deleteBtn);
             courseDiv.appendChild(controlsDiv);
 
-            //Span-element som avgränsning
+            //Span element som avdelare
             const span = <HTMLSpanElement>document.createElement('span');
             span.classList.add('divider');
             courseDiv.appendChild(span);
+
             courseList.appendChild(courseDiv);
-
-            //Händelselyssnare på knapparna
-            //ÄNDRA KURS
-            editBtn.addEventListener('click', (e: Event) => {
-                //fyll i inputfälten med värden från kursen
-                loadValues(kurs);
-                //Ändra editing till true
-                isEditing = true;
-                //Ändra knapptext
-                submitBtn.value = 'Spara ändringar';
-                //Ta bort addcourse från knapp, lägg till changecourse istället
-                submitBtn.removeEventListener('click', addCourse);
-                submitBtn.addEventListener('click', () => {
-                    changeCourse(e, kurs);
-                });
-            });
-
-            // TA BORT KURSER
-            deleteBtn.addEventListener('click', () => {
-                //Hitta index som matchar det index vi har tilldelat kursen
-                const deleteIndex = courses.findIndex(
-                    (course) => `course_${courses.indexOf(course)}` === courseId
-                );
-                //Klipp ut kursen ur arrayen
-                courses.splice(deleteIndex, 1);
-                //Uppdatera storage
-                localStorage.setItem('courses', JSON.stringify(courses));
-                //Ta bort div som tillhör kurs
-                courseDiv.remove();
-            });
         });
     }
 }
 
-//Fyll i inputfält med värdena från kursen
-function loadValues(kurs: CourseInterface): void {
+/* Funktion för att fylla i värden */
+function loadValuesForEdit(kurs: CourseInterface): void {
     courseCodeInput.value = kurs.kurskod;
     courseNameInput.value = kurs.kursnamn;
     progressionInput.value = kurs.progression;
     coursePlanInput.value = kurs.kursplan;
 }
 
-// Funktion för att skriva över befintlig kurs med ny information
-function changeCourse(e: Event, kurs: CourseInterface) {
-    e.preventDefault();
-    //Lagra värden
-    let kurskod: string = courseCodeInput.value;
-    let kursnamn: string = courseNameInput.value;
-    let progression: string = progressionInput.value.toUpperCase();
-    let kursplan: string = coursePlanInput.value;
+/* Funktion för att uppdatera kurs */
+function updateCourse(): void {
+    // Om det inte finns en kurs att redigera, avbryt på en gång
+    if (editingCourseIndex === null) return;
 
-    // Hitta index för den kurs som redigeras
-    const index: number = courses.findIndex((course) => course.kurskod === kurs.kurskod);
+    //Lagra värdena från input
+    const kurskod: string = courseCodeInput.value;
+    const kursnamn: string = courseNameInput.value;
+    const progression: string = progressionInput.value.toUpperCase();
+    const kursplan: string = coursePlanInput.value;
 
-    // Uppdatera kursen med nya värden
-    courses[index] = {
+    // Validera inmatning
+    if (!validateInput(kurskod, kursnamn, progression, kursplan)) {
+        return;
+    }
+
+    // Uppdatera kursen med rätt index
+    courses[editingCourseIndex] = {
         kurskod,
         kursnamn,
         progression,
         kursplan,
     };
-
-    // Uppdatera LocalStorage med de nya kursvärdena
     localStorage.setItem('courses', JSON.stringify(courses));
 
-    // Återställ formuläret och övriga inställningar efter att ändringarna har sparats
-    isEditing = false;
-    console.log(isEditing);
-    submitBtn.value = 'Lägg till kurs';
-    submitBtn.addEventListener('click', addCourse); // Lägg tillbaka händelselyssnare för att lägga till kurs
-    printCourses(); // Uppdatera listan med kurser
+    // Nollställ fält
+    resetFields();
+    //Uppdatera kurslistan
+    printCourses();
+
+    // Återställ redigeringsindex
+    editingCourseIndex = null;
+    //Ändra knapp
+    submitBtn.innerHTML = 'Lägg till kurs';
 }
 
+// Återställ fälten i formuläret
 function resetFields(): void {
+    //Töm input
     courseCodeInput.value = '';
     courseNameInput.value = '';
     progressionInput.value = '';
     coursePlanInput.value = '';
 
-    error1.innerHTML = '';
-    error2.innerHTML = '';
-    error3.innerHTML = '';
-    error4.innerHTML = '';
+    //Töm error
+    error1.textContent = '';
+    error2.textContent = '';
+    error3.textContent = '';
+    error4.textContent = '';
 
-    courseCodeInput.select();
+    //Flytta caret till första input
+    courseCodeInput.focus();
 }
